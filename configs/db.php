4fbd5f77746973509f1591edf1becb9f92a70a00
IvanTsivPHP <78473106+IvanTsivPHP@users.noreply.github.com>
2021-02-03 16:21:43 +0700

    Delete configs directory

--- configs/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-return ['driver'    => 'mysql',
-    'host'      => 'localhost',
-    'database'  => 'blog',
-    'username'  => 'root',
-    'password'  => 'root',
-    'charset'   => 'utf8',
-    'collation' => 'utf8_unicode_ci',
-    'prefix'    => ''];
